c130133fc6264ddff1c7b7266678816677ef9453
Sergey Kukharenko <iamksergey@mail.ru>
2022-11-29 19:46:14 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const btnMenu = $('.button-menu');
+  const body = $('body');
+  const header = $('.header');
+  const menuItem = $('.js-menu-item');
+  const menuTglClsNm = 'show-menu';
+  const menuItemTglClsNm = 'active';
+
+  btnMenu.click(function () {
+    body.toggleClass(menuTglClsNm);
+  });
+
+  menuItem.click(function (e) {
+    if (body.hasClass(menuTglClsNm)) body.removeClass(menuTglClsNm);
+    $(this).addClass(menuItemTglClsNm).siblings().removeClass(menuItemTglClsNm);
+
+    const id = '#' + $(this).attr('data-href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - $(header).outerHeight()
+    }, 1000);
+    return false;
+  });
+});
